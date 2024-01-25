ba1b2136a893b514f61b4b6a2123b20b127e341b
Androbar <androbar115@gmail.com>
2024-01-24 23:52:33 -0300

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import styles from "./page.module.css";
+// import { PrismaClient } from '@prisma/client';
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <a href="http://google.com">google</a>
+    </main>
+  );
+}
